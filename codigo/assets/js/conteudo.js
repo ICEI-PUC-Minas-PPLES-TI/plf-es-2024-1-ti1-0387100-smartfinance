6f0538c812aaa8d8708bc90bc1ada24459c2f077
henriquec18 <henriqueslcarneiro18@gmail.com>
2024-06-30 23:17:33 -0300

    Atualizando página Content, colocando a NavBar e integrando

--- codigo/assets/js/conteudo.js
+++ codigo/assets/js/conteudo.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('db.json')
+    fetch('../db/db.json')
         .then(response => response.json())
         .then(data => {
             const criadoresFinanceira = document.getElementById('criadores-financeira');
@@ -89,3 +89,68 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Erro ao carregar os dados:', error));
 });
+document.addEventListener("DOMContentLoaded", function() {
+    let sidebar = document.querySelector(".sidebar");
+    let closeBtn = document.querySelector("#btn");
+    let homeLink = document.querySelector("#home-link");
+    let navList = document.querySelector(".nav-list");
+  
+    closeBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+        menuBtnChange();
+        toggleHomeLink();
+        toggleNavList();
+        toggleBtnCenter();
+    });
+  
+    homeLink.addEventListener("click", (e) => {
+      if (!sidebar.classList.contains("open")) {
+        e.preventDefault(); 
+      } else {
+        sidebar.classList.add("open");
+        menuBtnChange();
+      }
+    });
+  
+    function toggleHomeLink() {
+      if (sidebar.classList.contains("open")) {
+        homeLink.classList.remove("disabled-link");
+      } else {
+        homeLink.classList.add("disabled-link");
+      }
+    }
+  
+    function toggleNavList() {
+      if (sidebar.classList.contains("open")) {
+        navList.classList.remove("hide");
+      } else {
+        navList.classList.add("hide");
+      }
+    }
+  
+    function toggleBtnCenter() {
+      if (window.innerWidth <= 420) {
+        if (sidebar.classList.contains("open")) {
+          closeBtn.classList.remove("center");
+        } else {
+          closeBtn.classList.add("center");
+        }
+      } else {
+        closeBtn.classList.remove("center");
+      }
+    }
+  
+    toggleHomeLink();
+    toggleNavList();
+    toggleBtnCenter();
+  
+    function menuBtnChange() {
+        if (sidebar.classList.contains("open")) {
+            closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); 
+        } else {
+            closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+        }
+    }
+  
+    window.addEventListener('resize', toggleBtnCenter);
+  });
